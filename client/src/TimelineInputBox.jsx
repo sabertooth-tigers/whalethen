f6b631b81e56ee7dff81c2fff9eb408092f49fcc
Justin <jvcruz@ucdavis.edu>
2017-12-02 11:41:32 -0800

    Inputs clean up and prop types

--- client/src/TimelineInputBox.jsx
+++ client/src/TimelineInputBox.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const TimelineInputBox = (props) => (
+const TimelineInputBox = ({ onInputChange }) => (
   <div className="inputBox label">
     <label className="timelineName" htmlFor="timelineName">
     Timeline Name:
@@ -9,7 +9,7 @@ const TimelineInputBox = (props) => (
       id="timelineName"
       type="text"
       name="timelineName"
-      onChange={({ target }) => props.onInputChange(target.name, target.value)}
+      onChange={({ target }) => onInputChange(target.name, target.value)}
       placeholder="enter a name"
     />
     </label>
