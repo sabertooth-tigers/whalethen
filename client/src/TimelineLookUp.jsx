69f29926631883698ced7d659fed9f956f87bc9e
Justin <jvcruz@ucdavis.edu>
2017-12-02 12:03:40 -0800

    TimelineLookUp linter and proptype cleanup

--- client/src/TimelineLookUp.jsx
+++ client/src/TimelineLookUp.jsx
@@ -5,11 +5,14 @@ const TimelineLookUp = (props) => {
   const {
     getTrip,
     setId,
-    timelineId
+    timelineId,
   } = props;
-  let id;
 
-  const isEnter = (key, value) => key === 'Enter' ? getTrip(value) : 1;
+  const isEnter = (key, value) => {
+    if (key === 'Enter') {
+      getTrip(value);
+    }
+  };
 
   return (
     <div className="inputBox">
@@ -23,17 +26,16 @@ const TimelineLookUp = (props) => {
         placeholder="enter ID"
         onKeyUp={({ key, target }) => isEnter(key, target.value)}
       />
-      <button className="searchSubmit" onClick={() => getTrip(timelineId)}>Search ID</button>
+        <button className="searchSubmit" onClick={() => getTrip(timelineId)}>Search ID</button>
       </label>
     </div>
   );
 };
 
 TimelineLookUp.propTypes = {
-  handleID: propTypes.func.isRequired,
-  handleName: propTypes.func.isRequired,
-  onLookupEnter: propTypes.func.isRequired,
+  setId: propTypes.func.isRequired,
   getTrip: propTypes.func.isRequired,
+  timelineId: propTypes.string.isRequired,
 };
 
 export default TimelineLookUp;
