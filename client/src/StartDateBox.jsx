245352af713788228daae9089b916de95c596078
Mike Hernandez <mkhern@gmail.com>
2017-12-02 16:01:07 -0800

    Removed consolelog

--- client/src/StartDateBox.jsx
+++ client/src/StartDateBox.jsx
@@ -28,7 +28,6 @@ class StartDateBox extends React.Component {
   }
 
   onSelectChange(date) {
-    console.log(date);
     if (date.eventType === 3) {
       const rawDayCount = moment(date.end).diff(moment(date.start), 'days');
       this.props.setDateRange(rawDayCount);
@@ -70,7 +69,7 @@ class StartDateBox extends React.Component {
     const reply = () => {
       const ziriOpenCal = new SpeechSynthesisUtterance(`Whale then! Please select the start and end dates for your timeline.`);
       window.speechSynthesis.speak(ziriOpenCal);
-    }
+    };
     return (
       <div>
         <button onClick={this.handleButtonClick}>Or say "Pick a Date"</button>
@@ -81,37 +80,6 @@ class StartDateBox extends React.Component {
     );
   }
 }
-
-
-// const StartDateBox = ({ onInputChange, onEnter }) => {
-//   const today = new Date();
-//   const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
-//   const CalendarWithRange = withRange(Calendar);
-//   return (
-//     <div className="inputBox label">
-//       <label className="startDate" htmlFor="startDate">
-//       Start Date:
-//        <InfiniteCalendar
-//           width={1000}
-//           height={420}
-//           Component={CalendarWithRange}
-//           selected={null}
-//           onSelect={date => console.log(date)}
-//           locale={{
-//             headerFormat: 'MMM Do',
-//           }}
-//         />,
-//       {/* <input
-//         id="startDate"
-//         type="date"
-//         name="startDate"
-//         onChange={({ target }) => onInputChange(target.name, target.value)}
-//         placeholder="enter a start date"
-//       /> */}
-//       </label>
-//     </div>
-//   );
-// };
 
 StartDateBox.propTypes = {
   setDateRange: propTypes.func.isRequired,
